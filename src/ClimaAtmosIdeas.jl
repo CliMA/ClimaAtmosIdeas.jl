b39ef3fc51a741b702b697d776c168c708cc6a54
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-10-12 13:21:10 -0700

    Migrate from ClimaAtmos

---
+++ src/ClimaAtmosIdeas.jl
@@ -0,0 +1,8 @@
+module ClimaAtmosIdeas
+include("Domains/Domains.jl")
+include("BoundaryConditions/BoundaryConditions.jl")
+include("Models/Models.jl")
+include("Callbacks/Callbacks.jl")
+include("Simulations/Simulations.jl")
+include("Utils/Utils.jl")
+end
---
+++ src/Models/style_cache.jl
@@ -0,0 +1,10 @@
+
+"""
+Supertype for all model cache styles.
+"""
+abstract type AbstractCacheStyle <: AbstractModelStyle end
+
+struct CacheEmpty <: AbstractCacheStyle end
+struct CacheBase <: AbstractCacheStyle end
+struct CacheZeroMomentMicro <: AbstractCacheStyle end
+struct CacheOneMomentMicro <: AbstractCacheStyle end
